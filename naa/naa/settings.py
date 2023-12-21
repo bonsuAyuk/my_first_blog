c727c5b13828149c98f6c404cc30d4b1878f32c5
bonsuAyuk <bonsufr@gmail.com>
2023-12-21 11:32:14 +0100

    modifiesd template to show post from database

--- naa/naa/settings.py
+++ naa/naa/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)%51of^d%8%0zgww+u2ye!6wq0m9fc$j*x*$umwexsb@tmwr^b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
